5a7e44549d27fe230d1df22f8592eaacd3b5ce38
Amritha Varshini <aa0018@srmist.edu.in>
2026-09-12 10:50:13 +0530

    added

---
+++ AS2/P1/Main.java
@@ -0,0 +1,16 @@
+package AS2.P1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter PIN: ");
+        String pin = sc.nextLine();
+
+        if (pin.length() != 4)
+            System.out.println("Invalid PIN — must be exactly 4 digits.");
+        else
+            System.out.println("PIN length OK.");
+    }
+}
